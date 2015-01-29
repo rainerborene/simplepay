7e2e7f70750625e85f74254548f40a4d376236e5
Rainer Borene <rainerborene@gmail.com>
2015-01-29 16:25:53 -0200

    Add more tests.

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -39,15 +39,27 @@ describe SimplePay::DB do
     end
   end
 
-  describe '#add' do
+  describe '#add?' do
     it 'create a new credit card for a given name, card number, and limit' do
       item = ['Tom', 4111111111111111, '$1000']
-      record = subject.add(name: item[0], number: item[1], limit: item[2])
+      record = subject.add?(name: item[0], number: item[1], limit: item[2])
       record.name.must_equal item[0]
       record.number.must_equal item[1]
       record.limit.must_equal 1000
       record.must_be :valid?
     end
+  end
+
+  describe '#charge' do
+
+  end
+
+  describe '#credit' do
+
+  end
+
+  describe '#to_txt' do
+
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,8 @@ class MiniTest::Spec
   def described_class
     @described_class ||= self.class.ancestors.select {|c| c.respond_to? :desc }[-2].desc
   end
+
+  def mock(*args)
+    MiniTest::Mock.new(*args)
+  end
 end
